dd8cf0d5988124079ddfb05cf20ca2c55b10faf1
RaulORios <raul.orios89@outlook.com>
2021-10-14 13:19:17 -0300

    Arquivos atualizados

---
+++ src/Heranca_3_Orient_Objeto/Entities/SavingsAccount.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Heranca_3_Orient_Objeto.Entities
+{
+    class SavingsAccount: Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance,double interestRate):base(number,holder,balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            //Balance -= amount; (Dessa forma não traz as alterações da superclasse)
+            //Na solução abaixo, traz o metodo da superclasse e é possivel acrescentar mais coisas.
+            base.Withdraw(amount);
+            Balance -= 2.0;
+
+        }
+
+    }
+}
